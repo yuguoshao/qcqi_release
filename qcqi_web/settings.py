91352885340985aaa88238e3a0e6590819ae5980
yuguo <719812710@qq.com>
2021-03-27 21:08:02 +0800

    v5.0(mysql)

--- qcqi_web/settings.py
+++ qcqi_web/settings.py
@@ -113,10 +113,21 @@ WSGI_APPLICATION = 'qcqi_web.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_app',
+        'USER': 'root',
+        'PASSWORD': 'HUSTsvo123456',
+        'HOST': 'db',
+        'PORT': '3306',
+        'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
 
